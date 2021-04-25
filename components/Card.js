884e49b2249a0b570f9c79e66292b1de818ab47d
Sofia Ferro <sofiferro89@gmail.com>
2021-04-24 22:04:01 -0300

    flip fix

--- components/Card.js
+++ components/Card.js
@@ -1,16 +1,32 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import ReactCardFlip from "react-card-flip";
 const back = "/img/back.jpg";
 
 function Card() {
-  const [name, setName] = useState("");
+  const [tarotPeck, setTarotPeck] = useState([]);
+  const cardInitState = {
+    name: "",
+    message: "",
+    image: "",
+    imageOr: ""
+  } 
+  const [card, setCard] = useState(cardInitState);
+  const [isFlipped, setIsFlipped] = useState(false);
+  /*   const [name, setName] = useState("");
   const [message, setMessage] = useState("");
   const [image, setImage] = useState("");
   const [imageOr, setImageOr] = useState("");
-  const [loading, setLoading] = useState(0);
-  const [isFlipped, setIsFlipped] = useState(false);
+  const [loading, setLoading] = useState(0);*/
 
-  /*   useEffect(() => {
+  useEffect(() => {
+    fetch("https://tarot-purpura-api.vercel.app/cards.json")
+      .then((res) => res.json())
+      .then((cardPeck) => {
+        return setTarotPeck(cardPeck.cards);
+      });
+  }, []);
+
+  /*useEffect(() => {
     if (loading !== 0) {
       async function getCards() {
         const id = Math.floor(Math.random() * 78) + 1;
@@ -37,31 +53,7 @@ function Card() {
 
       getCards();
     }
-  }, [loading]); */
-
-  function getCards() {
-    const id = Math.floor(Math.random() * 78) + 1;
-    console.log(`card ${id}`);
-    fetch(`https://tarot-purpura-api.vercel.app/cards/${id}.json`)
-      .then((res) => res.json())
-      .then((cardPeck) => {
-        return getCardInfo(cardPeck.cards[0]);
-      })
-      .then(() => {
-        return setIsFlipped(!isFlipped);
-      })
-  }
-
-  function getCardInfo(card) {
-    const orientation = Math.floor(Math.random() * 2);
-    const img = `/img/cards/${card.img}`;
-    setImage(img);
-    orientation === 0 ? setImageOr("up") : setImageOr("down");
-    orientation === 0
-      ? setMessage(card.meaning_up)
-      : setMessage(card.meaning_rev);
-    setName(card.name);
-  }
+  }, [loading]);
 
   function handleGetCard() {
     getCards();
@@ -73,12 +65,25 @@ function Card() {
     setMessage(() => message === "");
     setName(() => name === "");
     setImageOr(() => imageOr === "")
+  } */
+  function getCard() {
+    const id = Math.floor(Math.random() * 78);
+    setCard(tarotPeck[id]);
+    setIsFlipped(() => !isFlipped)
+  }
+  function handleGetCard() {
+    getCard();
+    console.log(card)
+  }
+
+  function handleBackToDeck() {
+    setIsFlipped(() => !isFlipped);
   }
 
   return (
     <div className="card-container">
       <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
-        <img
+         <img
           alt=""
           src={back}
           className="card-img"
@@ -86,13 +91,13 @@ function Card() {
         />
         <img
           alt=""
-          src={image}
-          className={imageOr === "down" ? "reversed card-img" : "card-img"}
+          src={card.img ? `/img/cards/${card.img}` : ""}
+          className={card.imageOr === "down" ? "reversed card-img" : "card-img"}
           onClick={() => handleBackToDeck()}
         />
       </ReactCardFlip>
-      <h4 className="name">{name}</h4>
-      <p className="message">{message}</p>
+      <h4 className="name">{card.name}</h4>
+      <p className="message">{card.imageOr === "down" ? card.meaning_rev : card.meaning_up}</p>
     </div>
   );
 }
