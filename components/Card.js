f4493ce761f03a41e0ced95b0633a879d40db479
Sofia Ferro <sofiferro89@gmail.com>
2021-04-25 20:18:48 -0300

    flip fix

--- components/Card.js
+++ components/Card.js
@@ -4,19 +4,9 @@ const back = "/img/back.jpg";
 
 function Card() {
   const [tarotPeck, setTarotPeck] = useState([]);
-  const cardInitState = {
-    name: "",
-    message: "",
-    image: "",
-    imageOr: ""
-  } 
-  const [card, setCard] = useState(cardInitState);
+  const [card, setCard] = useState({});
+  const [meaning, setMeaning] = useState("");
   const [isFlipped, setIsFlipped] = useState(false);
-  /*   const [name, setName] = useState("");
-  const [message, setMessage] = useState("");
-  const [image, setImage] = useState("");
-  const [imageOr, setImageOr] = useState("");
-  const [loading, setLoading] = useState(0);*/
 
   useEffect(() => {
     fetch("https://tarot-purpura-api.vercel.app/cards.json")
@@ -28,34 +18,26 @@ function Card() {
 
   function getCard() {
     const id = Math.floor(Math.random() * 78);
+    const orientation = Math.floor(Math.random() * 2);
+    orientation === 0 ? setMeaning("up") : setMeaning("rev");
     setCard(tarotPeck[id]);
-    setIsFlipped(()=> !isFlipped)
-  }
-  function handleGetCard() {
-    getCard();
-    console.log(card)
-
-  }
-
-  function handleBackToDeck() {
     setIsFlipped(() => !isFlipped);
   }
 
-/*   function getCardInfo(card) {
-    const orientation = Math.floor(Math.random() * 2);
-    const img = `/img/cards/${card.img}`;
-    setImage(img);
-    orientation === 0 ? setImageOr("up") : setImageOr("down");
-    orientation === 0
-      ? setMessage(card.meaning_up)
-      : setMessage(card.meaning_rev);
-    setName(card.name)
-  } */
+  function handleGetCard() {
+    getCard();
+    console.log(card);
+  }
+
+  function handleBackToDeck() {
+    setCard({})
+    setIsFlipped(() => !isFlipped)
+  }
 
   return (
     <div className="card-container">
       <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
-         <img
+        <img
           alt=""
           src={back}
           className="card-img"
@@ -64,12 +46,14 @@ function Card() {
         <img
           alt=""
           src={card.img ? `/img/cards/${card.img}` : ""}
-          className={card.imageOr === "down" ? "reversed card-img" : "card-img"}
+          className={card.meaning === "rev" ? "reversed card-img" : "card-img"}
           onClick={() => handleBackToDeck()}
         />
       </ReactCardFlip>
       <h4 className="name">{card.name}</h4>
-      <p className="message">{card.imageOr === "down" ? card.meaning_rev : card.meaning_up}</p>
+      <p className="message">
+        {card.meaning === "rev" ? card.meaning_rev : card.meaning_up}
+      </p>
     </div>
   );
 }
